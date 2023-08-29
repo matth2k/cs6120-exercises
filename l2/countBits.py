2c4a2047646e1aa4cbf34869fe4bd443b3c467fe
matth2k <mrh259@cornell.edu>
2023-08-29 12:38:40 -0400

    calculate with duplicates removed

--- l2/countBits.py
+++ l2/countBits.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     brilProgram = json.load(args.input)
+    constPresent = set()
 
     # Maybe you to know how many bits are needed for the constants in your program.
     constantAllocSpace = 0
@@ -17,11 +18,22 @@ if __name__ == "__main__":
         for insn in func["instrs"]:
             if "op" in insn and insn["op"] == "const":
                 # Bril ints are 64b
-                if insn["type"] == "int":
+                if insn["type"] == "int" or insn["type"] == "float":
                     constantAllocSpace += 64
+                    constPresent.add((insn["value"], 64))
                 elif insn["type"] == "bool":
                     constantAllocSpace += 1
+                    constPresent.add((insn["value"], 1))
 
     print(
         f"There are {constantAllocSpace} bits-worth of constants used in the program."
+    )
+
+    # Now calculate, subtracting repeated values
+    constantAllocSpace = 0
+    for const in constPresent:
+        constantAllocSpace += const[1]
+
+    print(
+        f"If you remove duplicates values, only {constantAllocSpace} bits-worth of constants used in the program."
     )
