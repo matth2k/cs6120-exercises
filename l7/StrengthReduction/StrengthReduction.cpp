658e0592d74b77bf9b2123b0d5423b7aa3f97ddb
matth2k <mrh259@cornell.edu>
2023-10-02 16:59:33 -0400

    do strengthen reduction for multiply

--- l7/StrengthReduction/StrengthReduction.cpp
+++ l7/StrengthReduction/StrengthReduction.cpp
@@ -2,17 +2,63 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
+#include <cassert>
+#include <cstdint>
+#include <llvm-15/llvm/IR/Constants.h>
+#include <llvm-15/llvm/IR/DerivedTypes.h>
+#include <llvm-15/llvm/IR/InstrTypes.h>
+#include <llvm-15/llvm/IR/Operator.h>
+#include <llvm-15/llvm/Support/MathExtras.h>
+#include <llvm-15/llvm/Support/raw_ostream.h>
 
 using namespace llvm;
 
+// Page 240 of Hsu, 2021 helped me with this
 namespace {
 
 struct StrengthReductionPass : public PassInfoMixin<StrengthReductionPass> {
+  // Strength reduction patterns
+
+  void rewriteMul2Shl(BinaryOperator &mul) {
+    auto lhs = dyn_cast<ConstantInt>(mul.getOperand(0));
+    auto rhs = dyn_cast<ConstantInt>(mul.getOperand(1));
+    if (!((lhs != nullptr && isPowerOf2_64(lhs->getValue().getZExtValue())) ||
+          (rhs != nullptr && isPowerOf2_64(rhs->getValue().getZExtValue()))))
+      return;
+    auto shift_amt =
+        (lhs != nullptr && isPowerOf2_64(lhs->getValue().getZExtValue()))
+            ? Log2_64(lhs->getValue().getZExtValue())
+            : Log2_64(rhs->getValue().getZExtValue());
+    auto operand =
+        (lhs != nullptr && isPowerOf2_64(lhs->getValue().getZExtValue()))
+            ? mul.getOperand(1)
+            : mul.getOperand(0);
+    // Idk if I'm even handing signed and unsigned types right, just getting the
+    // pass to work
+    auto intType = mul.getType();
+    IRBuilder<> builder(&mul);
+    auto *shl =
+        builder.CreateShl(operand, ConstantInt::get(intType, shift_amt));
+    mul.replaceAllUsesWith(shl);
+    mul.eraseFromParent();
+  }
+
   PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
+    SmallVector<BinaryOperator *> mul2Rewrite;
+    llvm::errs() << "let's go\n";
     for (auto &F : M) {
-      errs() << "I saw a function called " << F.getFunction().getName()
-             << "!\n";
+      for (auto &BB : F.getBasicBlockList()) {
+        for (auto &I : BB.getInstList()) {
+          if (auto *mul = dyn_cast<MulOperator>(&I)) {
+            mul2Rewrite.push_back(&cast<BinaryOperator>(I));
+          }
+        }
+      }
     }
+
+    for (auto &mul : mul2Rewrite)
+      rewriteMul2Shl(*mul);
+    llvm::errs() << "done\n";
     return PreservedAnalyses::all();
   };
 };
