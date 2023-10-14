b4599e7f7c28dc14bf2759dcae268085aeb77e32
matth2k <mrh259@cornell.edu>
2023-10-14 18:12:36 -0400

    change boilerplate to use old pass manager

--- l8/LoopUnswitch/LoopUnswitch.cpp
+++ l8/LoopUnswitch/LoopUnswitch.cpp
@@ -4,76 +4,35 @@
 #include "llvm/Support/raw_ostream.h"
 #include <cassert>
 #include <cstdint>
+#include <llvm-15/llvm/Analysis/LoopPass.h>
 #include <llvm-15/llvm/IR/Constants.h>
 #include <llvm-15/llvm/IR/DerivedTypes.h>
 #include <llvm-15/llvm/IR/InstrTypes.h>
 #include <llvm-15/llvm/IR/Operator.h>
+#include <llvm-15/llvm/IR/PassManager.h>
 #include <llvm-15/llvm/Support/MathExtras.h>
 #include <llvm-15/llvm/Support/raw_ostream.h>
+#include <memory>
 
 using namespace llvm;
 
-// Page 240 of Hsu, 2021 helped me with this
+// References:
+// * Hsu, 2021
+// * llvm.org/docs/WritingAnLLVMPass.html
 namespace {
 
-struct StrengthReductionPass : public PassInfoMixin<StrengthReductionPass> {
+struct LoopUnswitchPass : public LoopPass {
   // Strength reduction patterns
+  static char ID;
+  LoopUnswitchPass() : LoopPass(ID) {}
 
-  void rewriteMul2Shl(BinaryOperator &mul) {
-    auto lhs = dyn_cast<ConstantInt>(mul.getOperand(0));
-    auto rhs = dyn_cast<ConstantInt>(mul.getOperand(1));
-    if (!((lhs != nullptr && isPowerOf2_64(lhs->getValue().getZExtValue())) ||
-          (rhs != nullptr && isPowerOf2_64(rhs->getValue().getZExtValue()))))
-      return;
-    auto shift_amt =
-        (lhs != nullptr && isPowerOf2_64(lhs->getValue().getZExtValue()))
-            ? Log2_64(lhs->getValue().getZExtValue())
-            : Log2_64(rhs->getValue().getZExtValue());
-    auto operand =
-        (lhs != nullptr && isPowerOf2_64(lhs->getValue().getZExtValue()))
-            ? mul.getOperand(1)
-            : mul.getOperand(0);
-    // Idk if I'm even handing signed and unsigned types right, just getting the
-    // pass to work
-    auto intType = mul.getType();
-    IRBuilder<> builder(&mul);
-    auto *shl =
-        builder.CreateShl(operand, ConstantInt::get(intType, shift_amt));
-    mul.replaceAllUsesWith(shl);
-    mul.eraseFromParent();
-  }
-
-  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
-    SmallVector<BinaryOperator *> mul2Rewrite;
-    llvm::errs() << "let's go\n";
-    for (auto &F : M) {
-      for (auto &BB : F.getBasicBlockList()) {
-        for (auto &I : BB.getInstList()) {
-          if (auto *mul = dyn_cast<MulOperator>(&I)) {
-            mul2Rewrite.push_back(&cast<BinaryOperator>(I));
-          }
-        }
-      }
-    }
-
-    for (auto &mul : mul2Rewrite)
-      rewriteMul2Shl(*mul);
-    llvm::errs() << "done\n";
-    return PreservedAnalyses::all();
-  };
+  virtual bool runOnLoop(Loop *L, LPPassManager &LPM) { return false; }
+  virtual bool doInitialization(Loop *L, LPPassManager &LPM) { return false; }
+  virtual bool doFinalization() { return false; }
 };
 
 } // namespace
-
-extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
-llvmGetPassPluginInfo() {
-  return {.APIVersion = LLVM_PLUGIN_API_VERSION,
-          .PluginName = "Loop Unswitching Pass for CS6120",
-          .PluginVersion = "v0.1",
-          .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
-            PB.registerPipelineStartEPCallback(
-                [](ModulePassManager &MPM, OptimizationLevel Level) {
-                  MPM.addPass(StrengthReductionPass());
-                });
-          }};
-}
+char LoopUnswitchPass::ID = 0;
+static RegisterPass<LoopUnswitchPass> P("loop-unswitch", "Loop Unswitch Pass",
+                                        false /* Only looks at CFG */,
+                                        false /* Analysis Pass */);
