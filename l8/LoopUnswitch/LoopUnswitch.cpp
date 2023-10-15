b963ad91bc5087b0319ae53f3fea7fb87c0c0c5a
matth2k <mrh259@cornell.edu>
2023-10-14 20:28:35 -0400

    boilerplate working for loop pass

--- l8/LoopUnswitch/LoopUnswitch.cpp
+++ l8/LoopUnswitch/LoopUnswitch.cpp
@@ -4,38 +4,49 @@
 #include "llvm/Support/raw_ostream.h"
 #include <cassert>
 #include <cstdint>
-#include <llvm-15/llvm/Analysis/LoopPass.h>
 #include <llvm-15/llvm/IR/Constants.h>
 #include <llvm-15/llvm/IR/DerivedTypes.h>
 #include <llvm-15/llvm/IR/InstrTypes.h>
 #include <llvm-15/llvm/IR/Operator.h>
 #include <llvm-15/llvm/IR/PassManager.h>
+#include <llvm-15/llvm/Passes/OptimizationLevel.h>
 #include <llvm-15/llvm/Support/MathExtras.h>
 #include <llvm-15/llvm/Support/raw_ostream.h>
-#include <memory>
+#include <llvm-15/llvm/Transforms/Scalar/LoopPassManager.h>
 
 using namespace llvm;
 
 // References:
 // * Hsu, 2021
-// * llvm.org/docs/WritingAnLLVMPass.html
+// * discourse.llvm.org/t/how-to-write-a-loop-pass-using-new-pass-manager/70240
 namespace {
 
-struct LoopUnswitchPass : public LoopPass {
-  // Strength reduction patterns
-  static char ID;
-  LoopUnswitchPass() : LoopPass(ID) {}
+struct LoopUnswitchPass : public PassInfoMixin<LoopUnswitchPass> {
 
-  virtual bool runOnLoop(Loop *L, LPPassManager &LPM) {
-    llvm::errs() << "loop name: " << L->getName() << "\n";
-    return false;
+  PreservedAnalyses run(Loop &L, LoopAnalysisManager &LAM,
+                        LoopStandardAnalysisResults &AR, LPMUpdater &U) {
+    llvm::errs() << "loop name: \n";
+    return PreservedAnalyses::all();
   }
-  virtual bool doInitialization(Loop *L, LPPassManager &LPM) { return false; }
-  virtual bool doFinalization() { return false; }
 };
 
 } // namespace
-char LoopUnswitchPass::ID = 0;
-static RegisterPass<LoopUnswitchPass> P("loop-unswitch", "Loop Unswitch Pass",
-                                        false /* Only looks at CFG */,
-                                        false /* Analysis Pass */);
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return {.APIVersion = LLVM_PLUGIN_API_VERSION,
+          .PluginName = "LoopUnswitchingPassCS6120",
+          .PluginVersion = "v0.1",
+          .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, LoopPassManager &LPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "loop-unswitch") {
+                    llvm::errs() << "loop unswitching pass for loop\n";
+                    LPM.addPass(LoopUnswitchPass());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
