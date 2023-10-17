5f131abd515cc74435cbc0cee25b9ea967d1dad6
matth2k <mrh259@cornell.edu>
2023-10-17 03:12:24 -0400

    don't use disassembler

--- l8/optCode.sh
+++ l8/optCode.sh
@@ -2,11 +2,10 @@
 set -e
 # The clang flags are super important to testing the optimization in isolation
 clang -S -O1 -Xclang -disable-llvm-passes -emit-llvm $1 -o $1.ll
-opt -load-pass-plugin build/LoopUnswitch/LoopUnswitchPass.so -passes="cgscc(inline),mem2reg,dse,function(loop(loop-unswitch))" $1.ll > $1.tmp 2> $1.moved
+opt -load-pass-plugin build/LoopUnswitch/LoopUnswitchPass.so -passes="cgscc(inline),mem2reg,dse,function(loop(loop-unswitch))" -S $1.ll > $1.ll.opt 2> $1.moved
 cat $1.moved
 echo "Number of instructions moved in $1:"
 cat $1.moved | grep "moving" | wc -l
-llvm-dis $1.tmp -o $1.ll.opt
 llc -O0 -filetype=obj $1.ll.opt -o $1.o
 clang -no-pie $1.o -o $1.out -lm
-rm $1.tmp $1.o $1.moved
+rm $1.o $1.moved
