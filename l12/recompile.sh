04b2354fe65bf4e36d3dc3b091be5a73ce483d81
matth2k <mrh259@cornell.edu>
2023-11-09 18:15:08 -0500

    add another interpreter to get commit abort data

--- l12/recompile.sh
+++ l12/recompile.sh
@@ -4,6 +4,6 @@ set -o pipefail
 echo "Before:"
 blocks=$(bril2json < $1 | deno run brili.ts $2 $3 $4 -p | python3 findHotPath.py -v 2> tmp.txt)
 echo "After:"
-bril2json < $1 | python3 insertTrace.py -v -b $blocks 2>> tmp.txt | brili $2 $3 $4 -p
+bril2json < $1 | python3 insertTrace.py -v -b $blocks 2>> tmp.txt | deno run brili-br.ts $2 $3 $4 -p
 echo "Transform Report:"
 cat tmp.txt
